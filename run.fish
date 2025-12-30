284ebfdcfc46ba456e4066c697c8c8152ca1ee7a
pnppl <pnppl>
2025-12-30 06:57:38 -0500

    separate 3p

--- run.fish
+++ run.fish
@@ -2,4 +2,6 @@ cat yaml.txt > uBlacklist.txt &&
 echo "# URLS" >> uBlacklist.txt &&
 sort urls.txt >> uBlacklist.txt &&
 echo -e "\n# handles" >> uBlacklist.txt &&
-python3 handles.py handles.ssv | sort >> uBlacklist.txt
+python3 handles.py handles.ssv | sort >> uBlacklist.txt &&
+echo -e "\n# third party" >> uBlacklist.txt &&
+python3 handles.py 3p.ssv | sort >> uBlacklist.txt
